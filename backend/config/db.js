f4fc32fe058527b8b0b75dd3f09d0bbe51f9d611
tallosim <tallosi.mate@simonyi.bme.hu>
2022-05-09 11:35:28 +0200

    Add .env DB_URL.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/VITMMA09')
 
 module.exports = mongoose

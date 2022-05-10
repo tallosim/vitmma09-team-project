34ecb51ef9734e1b17d8bf1698deb96db90724d3
tallosim <tallosi.mate@simonyi.bme.hu>
2022-05-10 23:19:20 +0200

    Create prover error messages.

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import {
 	Container,
 	Typography,
@@ -12,137 +12,129 @@ import Basket from './Basket'
 import { apiServices } from '../Services'
 import { isUUID } from '../Helpers'
 
-class App extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			basket: null,
-			loading: false,
-			videoInputDeviceId: null
-		}
+const codeReader = new BrowserQRCodeReader()
 
-		this.codeReader = new BrowserQRCodeReader()
+const ErrorView = ({ styles, errorMsg, reset }) => (
+	<div style={styles.error}>
+		<Typography style={styles.error.text} variant='h4'>
+			{errorMsg}
+		</Typography>
+		<Button variant='contained' size='large' style={styles.error.button} onClick={reset}>
+			Retry!
+		</Button>
+	</div>
+)
+
+const App = () => {
+	const styles = {
+		contanier: {
+			backgroundColor: '#282c34',
+			minHeight: '100vh',
+			color: '#ffffff',
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+		},
+		title: {
+			fontWeight: 'bold',
+			lineHeight: '1',
+			maxWidth: '70%',
+			margin: '5%',
+		},
+		circularProgress: {
+			marginTop: '70%'
+		},
+		copyright: {
+			justifyContent: 'flex-end',
+			position: 'fixed',
+			bottom: '3%'
+		},
+		camera: {
+			width: '80%',
+			maxHeigth: '70vh'
+		},
+		error: {
+			margin: '15px',
+			height: '70vh',
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			flexDirection: 'column',
+			text: {
+				fontVariant: 'small-caps',
+				textAlign: 'center',
+				maxWidth: '70%'
+			},
+			button: {
+				margin: '30px'
+			}
+		}
 	}
 
-	component
+	const [basket, setBasket] = useState(null)
+	const [loading, setLoading] = useState(false)
+	const [errorMsg, setErrorMsg] = useState(null)
 
-	render() {
-		const styles = {
-			contanier: {
-				backgroundColor: '#282c34',
-				minHeight: '100vh',
-				color: '#ffffff',
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-			},
-			title: {
-				fontWeight: 'bold',
-				lineHeight: '1',
-				maxWidth: '70%',
-				margin: '5%',
-			},
-			circularProgress: {
-				marginTop: '70%'
-			},
-			copyright: {
-				justifyContent: 'flex-end',
-				position: 'fixed',
-				bottom: '3%'
-			},
-			camera: {
-				width: '80%',
-				maxHeigth: '70vh'
-			},
-			invalidBasket: {
-				margin: '15px',
-				height: '70vh',
-				display: 'flex',
-				alignItems: 'center',
-				justifyContent: 'center',
-				flexDirection: 'column',
-				text: {
-					fontVariant: 'small-caps',
-					textAlign: 'center',
-					maxWidth: '70%'
-				},
-				button: {
-					margin: '30px'
-				}
-			}
-		}
+	const reset = () => {
+		setBasket(null)
+		setLoading(false)
+		setErrorMsg(null)
+	}
 
-		const { basket, loading } = this.state
-		const setBasket = (basket) => this.setState({ basket })
-		const setLoading = (loading) => this.setState({ loading })
+	const onResult = (result) => {
+		console.log('QR code scanned!')
+		console.log(result.text)
 
-		const reset = () => setBasket(null)
-
-		const onResult = (result) => {
-			console.log('QR code scanned!')
-			console.log(result.text)
-
-			if (result.text && isUUID(result.text) && !basket) {
-				setLoading(true)
-				apiServices.getProducts(result.text)
-					.then(response => {
-						setLoading(false)
-						setBasket(response.data)
-					})
-					.catch(reason => {
-						console.log('No bucket found!')
-						setBasket('INVALID')
-					})
-			}
-			else {
-				console.log('Not valid UUID!')
-				setBasket('INVALID')
-			}
-		}
-
-		const startScanning = mediaStream => setTimeout(() => {
-			const videoElement = document.querySelector('video')
-			if (videoElement) {
-				this.codeReader.decodeFromVideoElement(videoElement, (result, error, controls) => {
-					if (result) {
-						controls.stop()
-						onResult(result)
-					}
+		if (result.text && isUUID(result.text) && !basket) {
+			setLoading(true)
+			apiServices.getProducts(result.text)
+				.then(response => {
+					setLoading(false)
+					setBasket(response.data)
 				})
-			}
-		}, 100)
+				.catch(reason => {
+					console.log(reason)
 
-		return (
-			<Container style={styles.contanier}>
-				<Typography variant='h2' style={styles.title} align='center'>
-					Smart Basket
-				</Typography>
-				{!loading ?
-					!basket
-						? <Webcam style={styles.camera} onUserMedia={startScanning} />
-						: basket === 'INVALID'
-							? <div style={styles.invalidBasket}>
-								<Typography style={styles.invalidBasket.text} variant='h4'>
-									INVALID QR CODE
-								</Typography>
-								<Button
-									variant='contained'
-									size='large'
-									style={styles.invalidBasket.button}
-									onClick={reset}
-								>
-									Retry!
-								</Button>
-							</div>
-							: <Basket data={basket} reset={reset} />
-					: <CircularProgress size={80} style={styles.circularProgress} />
-				}
-				<Typography variant='h6' style={styles.copyright} align='center'>
-					Máté Tallósi &copy; 2022
-				</Typography>
-			</Container >
-		)
+					const message = reason.response
+						&& reason.response.status
+						&& reason.response.status === 404
+						? 'Basket not found this QR code.'
+						: reason.message
+					setLoading(false)
+					setErrorMsg(message)
+				})
+		}
+		else {
+			setErrorMsg('Invalid QR code.')
+		}
 	}
+
+	const startScanning = mediaStream => setTimeout(() => {
+		const videoElement = document.querySelector('video')
+		if (videoElement) {
+			codeReader.decodeFromVideoElement(videoElement, (result, error, controls) => {
+				if (result) {
+					controls.stop()
+					onResult(result)
+				}
+			})
+		}
+	}, 100)
+
+	return (
+		<Container style={styles.contanier}>
+			<Typography variant='h2' style={styles.title} align='center'>
+				Smart Basket
+			</Typography>
+			{basket && <Basket data={basket} reset={reset} />}
+			{!basket && !errorMsg && <Webcam style={styles.camera} onUserMedia={startScanning} />}
+			{loading && <CircularProgress size={100} style={styles.circularProgress} />}
+			{errorMsg && <ErrorView styles={styles} errorMsg={errorMsg} reset={reset} />}
+			<Typography variant='h6' style={styles.copyright} align='center'>
+				Máté Tallósi &copy; 2022
+			</Typography>
+		</Container >
+	)
 }
 
 export default App

93032756c62a3800910bee52d8ecc8498c997444
tallosim <tallosi.mate@simonyi.bme.hu>
2022-05-10 18:30:44 +0200

    Updates to Client Rendering APIs.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './Components/App'
 
 import './Assets/index.css'
 
-
-ReactDOM.render(
+createRoot(document.getElementById('app')).render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+	</React.StrictMode>
 )
